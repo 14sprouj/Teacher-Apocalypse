d087adc9afe29a2e79341f8e7766149188a511fd
Joel Sprouse <20050599@student.stcg.ac.uk>
2022-05-05 10:04:46 +0100

    health regeneration

--- js/damage.js
+++ js/damage.js
@@ -1,7 +1,8 @@
 var playerHealth = 100;
 
 function displayNewHealth(health) {
-	$("#playerHealthNumber").html(health + "%");
+	var healthPoints = health.toFixed(2);
+	$("#playerHealthNumber").html(parseFloat(healthPoints) + "%");
 	$("#playerHealthBarFill").css("width", health + "%");
 }
 
@@ -10,4 +11,15 @@ function deductPlayerHealth(points) {
 		playerHealth -= points;
 		displayNewHealth(playerHealth);
 	}
+	if (playerHealth < 0) {
+		playerHealth = 0;
+		displayNewHealth(playerHealth);
+	}
 }
+
+setInterval(function () {
+	if (playerHealth < 100) {
+		playerHealth += 0.1;
+		displayNewHealth(playerHealth);
+    }
+}, 500)
